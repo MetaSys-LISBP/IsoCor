0a7d1aa395514a4f1fbf7df98669da28dca314ad
millard <millard@insat.insa-toulouse.fr>
2018-10-14 15:00:50 +0200

    fix pdf doc

--- doc/conf.py
+++ doc/conf.py
@@ -191,6 +191,7 @@ latex_elements = {
 
 # remove blank pages (between the title page and the TOC, etc.)
 'classoptions': ',openany,oneside',
+'babel' : '\\usepackage[english]{babel}',
 
 # Additional stuff for the LaTeX preamble.
 'preamble': r'''
