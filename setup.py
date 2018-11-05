794a8104bb3df7b58e3a0bb9c7633c25e7dadfe5
Baudoin Delépine <baudoin.delepine@gmail.com>
2018-11-05 20:04:50 +0100

    chore: bump version forward

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Do not forget to update doc/conf.py version
+
 setuptools.setup(
     name="IsoCor",
-    version="2.0.0",
+    version="2.0.1",
     author="Pierre Millard, Baudoin Delépine, Matthieu Guionnet",
     author_email="millard@insa-toulouse.fr",
     description="IsoCor: Isotope Correction for mass spectrometry labeling experiments",
@@ -13,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MetaSys-LISBP/IsoCor/",
     packages=setuptools.find_packages(),
+    python_requires='>=3.4',
     install_requires=['pandas>=0.17.1', 'scipy>=0.12.1'],
     package_data={'': ['data/*.dat', ], },
     include_package_data=True,
