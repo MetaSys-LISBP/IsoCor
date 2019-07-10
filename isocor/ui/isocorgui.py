4d61be3ce752eaf526fe3490bebbe31d98da9ded
pierremillard <millard@insa-toulouse.fr>
2019-07-10 16:08:37 +0200

    update tooltip messages

--- isocor/ui/isocorgui.py
+++ isocor/ui/isocorgui.py
@@ -695,14 +695,14 @@ class GUIinterface(ttk.Frame):
         self.processButon.grid(column=0, row=1, columnspan=2, sticky='NWE')
         self.logstream.grid(column=0, row=2, columnspan=2, sticky='NWE')
         self.chVerboseLog.grid(column=0, row=3, sticky='NW')
-        tk.Label(content, text="infotip available over items with '(*)'").grid(column=0, row=4, columnspan=2, sticky='NE')
+        tk.Label(content, text="Note: infotip available over items with '(*)'").grid(column=0, row=4, columnspan=2, sticky='NE')
 
         # create tooltip helpers
-        Tooltip(self.chNatAbTracer, text="Correct for the contribution of naturaly occuring isotopes at unlabeled positions.")
-        Tooltip(self.R1, text="For measurements collected at unitary resolution (e.g. on Quad)")
-        Tooltip(self.R2, text="For measurements at high or ultrahigh resolution (e.g. on Orbitrap or FT-ICR)")
-        Tooltip(tr_lab, text="Correct for the contribution of isotopic impurities at labeled position. The purity is typically obtained from the manufacturer.\nE.g. for 13C substates with purity of 99% use 0.01 for 12C and 0.99 for 13C.")
-        Tooltip(self.chVerboseLog, text="Useful in case of trouble with the processed result. Join it to the issue on github")
+        Tooltip(self.chNatAbTracer, text="Correct for the contribution of naturally occurring isotopes at unlabeled positions.")
+        Tooltip(self.R1, text="For measurements collected at unitary resolution (e.g. on quadrupole instruments).")
+        Tooltip(self.R2, text="For measurements collected at high or ultrahigh resolution (e.g. on Orbitrap or FT-ICR instruments).")
+        Tooltip(tr_lab, text="Correct for the contribution of isotopic impurities at labeled position. The isotopic purity is typically obtained from the manufacturer.\ne.g. for 13C-substates with purity of 99%, use 0.01 for 12C and 0.99 for 13C.")
+        Tooltip(self.chVerboseLog, text="Useful in case of trouble with the processed result. Join it to the issue on github.")
         Tooltip(self.databasePathSubmit, text="Folder containing all database files.")
 
         # create texthandler and formatter to display logs
